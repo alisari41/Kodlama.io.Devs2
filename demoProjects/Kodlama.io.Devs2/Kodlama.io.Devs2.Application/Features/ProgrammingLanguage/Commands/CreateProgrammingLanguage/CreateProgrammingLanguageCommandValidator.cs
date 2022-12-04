18c8817aea5cdd52a0cdaa4462bc632dd7cd8f2f
alisari41 <alisari41@outlook.com>
2022-12-04 18:28:41 +0300

    Ekleme İşlemi İçin formatlamaların tamamlanması. ( FluentValidation)

---
+++ demoProjects/Kodlama.io.Devs2/Kodlama.io.Devs2.Application/Features/ProgrammingLanguage/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs2.Application.Features.ProgrammingLanguage.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        // FluentValidation ile Format Doğrulama işlemleri
+        // Ekeleme işlemleri için ayrı güncelleme işlemleri vs. ler için ayrı doğrulama işlemleri olabileceği için "CreateBrandCommand" ile ekleme işlemleri için yapıldı
+
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty(); // Boş Geçilemez
+        }
+    }
+}
