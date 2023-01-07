cc5e960a49b2a16525f46ae4cd97f12b5cfa07a8
alisari41 <alisari41@outlook.com>
2023-01-07 15:13:01 +0300

    JWT ( User - OperationClaim - UserOperationClaims - RefreshToken ) migration işlemleri tamamlandı.

--- src/demoProjects/Kodlama.io.Devs2/Kodlama.io.Devs2.Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/Kodlama.io.Devs2/Kodlama.io.Devs2.Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Kodlama.io.Devs2.Domain.Entities;
+﻿using Core.Security.Entities;
+using Kodlama.io.Devs2.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -10,6 +11,13 @@ public class BaseDbContext : DbContext
     public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
     public DbSet<Technology> Technologies { get; set; }
 
+
+    #region JWT (Json Web Token)
+    public DbSet<User> Users { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    #endregion
 
     public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
     {
@@ -51,6 +59,63 @@ public class BaseDbContext : DbContext
 
             #region Programlama Dili ile bağlantı
             a.HasOne(p => p.ProgrammingLanguage); // Bir teknolojinin Bir programlama dili olur 
+            #endregion
+        });
+
+        modelBuilder.Entity<User>(a =>
+        {
+            a.ToTable("Users").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.FirstName).HasColumnName("FirstName");
+            a.Property(p => p.LastName).HasColumnName("LastName");
+            a.Property(p => p.Email).HasColumnName("Email");
+            a.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+            a.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+            a.Property(p => p.Status).HasColumnName("Status");
+            a.Property(p => p.AuthenticatorType).HasColumnName("AuthenticatorType");
+
+            #region Diğer Tablolar ile bağlantı
+            a.HasMany(p => p.UserOperationClaims);
+            a.HasMany(p => p.RefreshTokens);
+            #endregion
+        });
+
+        modelBuilder.Entity<OperationClaim>(a =>
+        {
+            a.ToTable("OperationClaims").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.Name).HasColumnName("Name");
+        });
+
+        modelBuilder.Entity<UserOperationClaim>(a =>
+        {
+            a.ToTable("UserOperationClaims").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.UserId).HasColumnName("UserId");
+            a.Property(p => p.OperationClaimId).HasColumnName("OperationClaimId");
+
+            #region User ve OperationClaim ile Bağlantı
+            a.HasOne(p => p.User);
+            a.HasOne(p => p.OperationClaim);
+            #endregion
+        });
+
+        modelBuilder.Entity<RefreshToken>(a =>
+        {
+            a.ToTable("RefreshTokens").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.UserId).HasColumnName("UserId");
+            a.Property(p => p.Token).HasColumnName("Token");
+            a.Property(p => p.Expires).HasColumnName("Expires");
+            a.Property(p => p.Created).HasColumnName("Created");
+            a.Property(p => p.CreatedByIp).HasColumnName("CreatedByIp");
+            a.Property(p => p.Revoked).HasColumnName("Revoked");
+            a.Property(p => p.RevokedByIp).HasColumnName("RevokedByIp");
+            a.Property(p => p.ReplacedByToken).HasColumnName("ReplacedByToken");
+            a.Property(p => p.ReasonRevoked).HasColumnName("ReasonRevoked");
+
+            #region User ile Bağlantı
+            a.HasOne(p => p.User);
             #endregion
         });
 
